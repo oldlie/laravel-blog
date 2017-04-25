e5a984d7f9acc7fc96d3badb7c52e46d328a8ee0
oldlie <chsq00@163.com>
2017-04-25 14:09:29 +0800

    Fix Category issues

--- public/assets/js/ray.js
+++ public/assets/js/ray.js
@@ -27,6 +27,43 @@ var Core = (function () {
         xhr.open("POST", url);//修改成自己的接口
         xhr.send(formData);
     };
-    
+
+    Core.prototype.pagination = function(page, size, total, cells) {
+        var indexList = [];
+        var pageStart = 0;
+        var pageEnd = 0;
+        var pages = Math.ceil(total / size);
+
+        if (pages > cells) {
+            var mid = Math.ceil(cells / 2);
+
+            if (page > mid) {
+                if (page + mid >= pages + 1) {
+                    pageStart = pages - cells;
+                } else {
+                    pageStart = page - mid;
+                }
+            } else {
+                pageStart = 0;
+            }
+
+            if (page > pages - cells + 1) {
+                pageEnd = pages;
+            } else {
+                pageEnd = pageStart + cells;
+            }
+
+        } else {
+            pageStart = 0;
+            pageEnd = pages;
+        }
+
+        for (var i = pageStart; i < pageEnd; i++) {
+            indexList.push(i + 1);
+        }
+
+        return {"pages": pages, "list" : indexList};
+    };
+
     return Core;
 })();
